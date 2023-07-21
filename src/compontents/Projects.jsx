5cbdecc097b3b4edad5788c7d2189f9831120a3a
Jennifer Griffin <67939611+jengriffin@users.noreply.github.com>
2023-07-21 14:59:04 -0500

    changing to grid card display for projects

--- src/compontents/Projects.jsx
+++ src/compontents/Projects.jsx
@@ -6,110 +6,141 @@ import Latest from "./Latest";
 
 const Projects =()=>{
 return(
-    <section id="projects" class="bg-light">
-        <div class="container">
-            <Latest/>
-            <hr className="divider"/>
-    <a
-        class="btn btn-cta-secondary"
-        href="https://obscure-reef-40887.herokuapp.com/"
-        target="Recipes for Me"
-    >
-        <i class="fas fa-thumbs-up"></i>
-    </a>
-</div>
-    <hr class="divider" />
-    <div class="item row">
-        <a
-            class="col-md-4 col-12"
-            href="https://obscure-reef-40887.herokuapp.com/"
-            target="_blank"
-        >
-            <img
-                class="img-fluid project-image rounded shadow-sm"
-                src={cookbook}
-                alt="Recipes for Me"
-            />
-        </a>
-        <div class="desc col-md-8 col-12">
-            <h3 class="title">
-                <a
-                    href="https://obscure-reef-40887.herokuapp.com/"
-                    target="_blank"
-                >
-                    The Modern Cookbook!
-                </a>
-            </h3>
-            <p class="mb-2">
-                A deployed full stack PERN application built in 5 days in a dev group of 4 software engineers that allows registered users to create and share recipe skills.
-                My chief responsibilities were time management, working with the front-end team to build user authentication, protected routes, and user registration I also enforced good git hygiene on the front-end repo, communicating with the team to resolve merge conflicts, coordinate  pulls and pushes, and keeping the front-end up to date.
-                Registered users can create, edit, and delete recipes, creating the recipes available to all visitors, as well as links to food pantries in the About section to address the rising cost of health food. To check it out put in a fake email and password and you're on your way! Make sure to check out the other developers to see what exciting new creations they have in store! Cedric Mun <a href="https://github.com/Aonmonomer">GitHub</a><a href="https://www.linkedin.com/in/cedricmun/"> LinkedIn</a>, Josh Langner <a href="https://github.com/jlangner87">GitHub</a> <a href="https://www.linkedin.com/in/josh-langner/"> LinkedIn,</a> Jackson McHugh <a href="https://github.com/Jacksonmchugh">GitHub </a> <a href="https://www.linkedin.com/in/jackson-mchugh/">LinkedIn.</a>
-
-            </p>
-
+//     <section id="projects" class="bg-light">
+//         <div class="container">
+//             <Latest/>
+//             <hr className="divider"/>
+//     <a
+//         class="btn btn-cta-secondary"
+//         href="https://obscure-reef-40887.herokuapp.com/"
+//         target="Recipes for Me"
+//     >
+//         <i class="fas fa-thumbs-up"></i>
+//     </a>
+// </div>
+//     <hr class="divider" />
+//     <div class="item row">
+//         <a
+//             class="col-md-4 col-12"
+//             href="https://obscure-reef-40887.herokuapp.com/"
+//             target="_blank"
+//         >
+//             <img
+//                 class="img-fluid project-image rounded shadow-sm"
+//                 src={cookbook}
+//                 alt="Recipes for Me"
+//             />
+//         </a>
+//         <div class="desc col-md-8 col-12">
+//             <h3 class="title">
+//                 <a
+//                     href="https://obscure-reef-40887.herokuapp.com/"
+//                     target="_blank"
+//                 >
+//                     The Modern Cookbook!
+//                 </a>
+//             </h3>
+//             <p class="mb-2">
+//                 A deployed full stack PERN application built in 5 days in a dev group of 4 software engineers that allows registered users to create and share recipe skills.
+//                 My chief responsibilities were time management, working with the front-end team to build user authentication, protected routes, and user registration I also enforced good git hygiene on the front-end repo, communicating with the team to resolve merge conflicts, coordinate  pulls and pushes, and keeping the front-end up to date.
+//                 Registered users can create, edit, and delete recipes, creating the recipes available to all visitors, as well as links to food pantries in the About section to address the rising cost of health food. To check it out put in a fake email and password and you're on your way! Make sure to check out the other developers to see what exciting new creations they have in store! Cedric Mun <a href="https://github.com/Aonmonomer">GitHub</a><a href="https://www.linkedin.com/in/cedricmun/"> LinkedIn</a>, Josh Langner <a href="https://github.com/jlangner87">GitHub</a> <a href="https://www.linkedin.com/in/josh-langner/"> LinkedIn,</a> Jackson McHugh <a href="https://github.com/Jacksonmchugh">GitHub </a> <a href="https://www.linkedin.com/in/jackson-mchugh/">LinkedIn.</a>
+//
+//             </p>
+//
+//         </div>
+//     </div>
+//
+//     <div class="item row">
+//         <a
+//             class="col-md-4 col-12"
+//             href="https://robust-boasts.surge.sh/"
+//             target="_blank"
+//         >
+//             <img
+//                 class="img-fluid project-image rounded shadow-sm"
+//                 src={spaceman}
+//                 alt="Spaceman"
+//             />
+//         </a>
+//         <div class="desc col-md-8 col-12">
+//             <h3 class="title">
+//                 <a
+//                     href="https://robust-boasts.surge.sh/"
+//                     target="_blank"
+//                 >
+//                     Spaceman
+//                 </a>
+//             </h3>
+//             <p class="mb-2">
+//                 This front-end hangman game was built in 6 days utilizing JavaScript, CSS, and HTML 5 and is  structured to support early literacy and technology skills for children, covering 7 essential skills.
+//                 The words are the 100 most frequently read encountered words for kindergarten. High contrast colors, straight font styling and various element sizes make important features stand out for learners new to digital interactions and those with various levels of visual acuity.
+//                 The option to use a keyboard is given for learners who want to build keyboard awareness and those with motor disabilities.
+//
+//             </p>
+//
+//         </div>
+//     </div>
+//
+//     <div class="item row">
+//         <a
+//             class="col-md-4 col-12"
+//             href="https://fast-basin-96068.herokuapp.com/"
+//             target="_blank"
+//         >
+//             <img
+//                 class="img-fluid project-image rounded shadow-sm"
+//                 src={book}
+//                 alt="children's book database"
+//             />
+//         </a>
+//         <div class="desc col-md-8 col-12">
+//             <h3 class="title">
+//
+//             </h3>
+//             <p class="mb-2">
+//                 A full stack MERN application built in 6 days with 4 user interactive methods.
+//                 Built an Express server from scratch using Javascript, MongoDB, Mongoose HTML 5, CSS and React.
+//
+//             </p>
+//         </div>
+//     </div>
+//     </section>
+    <div className="card-group">
+        <div className="card">
+            <img className="card-img-top" src="..." alt="Card image cap"/>
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.</p>
+                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+        </div>
+        <div className="card">
+            <img className="card-img-top" src="..." alt="Card image cap"/>
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">This card has supporting text below as a natural lead-in to additional
+                        content.</p>
+                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+        </div>
+        <div className="card">
+            <img className="card-img-top" src="..." alt="Card image cap"/>
+                <div className="card-body">
+                    <h5 className="card-title">
+                        <a
+                        href="https://fast-basin-96068.herokuapp.com/"
+                        target="_blank">
+                        Children's Book Database
+                    </a>
+                    </h5>
+                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This card has even longer content than the first to show that equal height
+                        action.</p>
+                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                </div>
         </div>
     </div>
-
-    <div class="item row">
-        <a
-            class="col-md-4 col-12"
-            href="https://robust-boasts.surge.sh/"
-            target="_blank"
-        >
-            <img
-                class="img-fluid project-image rounded shadow-sm"
-                src={spaceman}
-                alt="Spaceman"
-            />
-        </a>
-        <div class="desc col-md-8 col-12">
-            <h3 class="title">
-                <a
-                    href="https://robust-boasts.surge.sh/"
-                    target="_blank"
-                >
-                    Spaceman
-                </a>
-            </h3>
-            <p class="mb-2">
-                This front-end hangman game was built in 6 days utilizing JavaScript, CSS, and HTML 5 and is  structured to support early literacy and technology skills for children, covering 7 essential skills.
-                The words are the 100 most frequently read encountered words for kindergarten. High contrast colors, straight font styling and various element sizes make important features stand out for learners new to digital interactions and those with various levels of visual acuity.
-                The option to use a keyboard is given for learners who want to build keyboard awareness and those with motor disabilities.
-
-            </p>
-
-        </div>
-    </div>
-
-    <div class="item row">
-        <a
-            class="col-md-4 col-12"
-            href="https://fast-basin-96068.herokuapp.com/"
-            target="_blank"
-        >
-            <img
-                class="img-fluid project-image rounded shadow-sm"
-                src={book}
-                alt="children's book database"
-            />
-        </a>
-        <div class="desc col-md-8 col-12">
-            <h3 class="title">
-                <a
-                    href="https://fast-basin-96068.herokuapp.com/"
-                    target="_blank"
-                >
-                    Children's Book Database
-                </a>
-            </h3>
-            <p class="mb-2">
-                A full stack MERN application built in 6 days with 4 user interactive methods.
-                Built an Express server from scratch using Javascript, MongoDB, Mongoose HTML 5, CSS and React.
-
-            </p>
-        </div>
-    </div>
-    </section>
 )
 
 }
