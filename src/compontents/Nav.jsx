515e83a4b318299d74cceef75a5176cd2ae1fc5f
Jennifer Griffin <67939611+jengriffin@users.noreply.github.com>
2023-07-21 11:45:17 -0500

    Updated links to Projects

--- src/compontents/Nav.jsx
+++ src/compontents/Nav.jsx
@@ -16,7 +16,7 @@ const Nav = () => {
                         <a className="nav-link" href="#">About <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Link</a>
+                        <a className="nav-link" href="#">Projects</a>
                     </li>
                     <li className="nav-item dropdown">
                         <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
