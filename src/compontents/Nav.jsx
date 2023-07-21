3d56d42794057765162f33a7abaef9f00f67c3ac
Jennifer Griffin <67939611+jengriffin@users.noreply.github.com>
2023-07-21 11:53:07 -0500

    Adding IDs is a success!

--- src/compontents/Nav.jsx
+++ src/compontents/Nav.jsx
@@ -1,4 +1,5 @@
-import Test from "./Test"
+import Projects from "./Projects";
+import Test from "./Test";
 const Nav = () => {
   return (
     <div className="nav">
@@ -16,13 +17,13 @@ const Nav = () => {
                         <a className="nav-link" href="#">About <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Projects</a>
+                        <a className="nav-link" href="#projects">Projects</a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="#">Skills</a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Testimonials</a>
+                        <a className="nav-link" href="#anchor_testimonials">Testimonials</a>
                     </li>
                 </ul>
             </div>
