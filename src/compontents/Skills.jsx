ecec42f33178d63ebb908b0c0190d66a806055a2
Jennifer Griffin <67939611+jengriffin@users.noreply.github.com>
2023-07-26 10:42:37 -0500

    Added skills style

--- src/compontents/Skills.jsx
+++ src/compontents/Skills.jsx
@@ -1,6 +1,10 @@
 const Skills = () => {
   return(
-    <img src="https://cr-skills-chart-widget.azurewebsites.net/api/api?username=jengriffin"/>
+      <div className="widget shadow p-3 mb-5 bg-body rounded">
+
+            <img class="img-fluid" src="https://cr-skills-chart-widget.azurewebsites.net/api/api?username=jengriffin"/>
+
+      </div>
   )
 }
 export default Skills
